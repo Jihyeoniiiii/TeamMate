a20759b7b09c2b79422d9b19671d43b5d889c7ec
현지현 <qwere4585@naver.com>
2024-11-30 19:19:50 +0900

    feat: create button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+const Button = ({ text }) => {
+  return (
+    <Wrapper>{text}</Wrapper>
+  )
+}
+
+const Wrapper = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-color: ${(props) => props.theme.colors.accent};
+    width: 100%;
+    height: 40px;
+    padding: 20px 0;
+    border-radius: 10px;
+    border: none;
+    font-weight: bold;
+    font-size: 16px;
+`
+
+export default Button
