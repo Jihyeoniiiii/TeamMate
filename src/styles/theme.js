30bd3c6eade65bd96339cc91da34cb0575c9f4ab
현지현 <qwere4585@naver.com>
2024-11-30 19:18:52 +0900

    feat: add globalstyles, theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primary: '#8583F7',
+        accent: '#423FE5',
+        lightgrey: '#EEEEEE',
+        grey: '#D9D9D9',
+        darkgrey: '#9C9C9C',
+    }
+}
