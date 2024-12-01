a2b4b37601c2b23bbf044d1fd853a5d713f13df0
현지현 <qwere4585@naver.com>
2024-12-01 14:43:54 +0900

    feat: create redux store, authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme.js'
 import App from './App.jsx'
 import GlobalStyles from './styles/GlobalStyles.js'
+import { Provider } from 'react-redux'
+import { store } from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <App />
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </Provider>
 )
