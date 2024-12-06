1face2513191bb3bd3b0f232f4166ac3cd7885b0
현지현 <qwere4585@naver.com>
2024-12-06 16:43:27 +0900

    feat: create apiError component

---
+++ src/utils/handleApiError.js
@@ -0,0 +1,18 @@
+export const handleApiError = (status, code) => {
+    const errorMessages = {
+      400: {
+        COMMON400: "잘못된 요청입니다. 입력값을 확인해주세요.",
+        UNIV4002: "인증이 불가능한 학교명입니다.",
+      },
+      404: {
+        UNIV4003: "재학생 인증에 실패하였습니다.",
+      },
+      409: {
+        UNIV4001: "회원정보가 이미 존재합니다. 로그인해주세요.",
+        UNIV4005: "인증 정보가 이미 존재합니다. 인증 코드 요청 없이 회원가입을 진행하세요.",
+      },
+    };
+  
+    return errorMessages[status]?.[code] || "알 수 없는 오류가 발생했습니다. 관리자에게 문의하세요.";
+  };
+  
