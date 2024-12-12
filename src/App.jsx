0b55ff2c8a377ccd9f3e30e03b7b71e6174154a9
현지현 <qwere4585@naver.com>
2024-12-12 17:11:05 +0900

    feat: separate Navigation Bar into Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectCreationPage from  "./pages/ProjectCreationPage"
+import Layout from "./layout/Layout";
 
 function App() { 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projectCreation" element={<ProjectCreationPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projectCreation" element={<ProjectCreationPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
    ); 
