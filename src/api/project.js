51bfb227099017e35bd0779d61be5eaa78f40f17
현지현 <qwere4585@naver.com>
2025-01-16 16:38:48 +0900

    refactor: auth, project api 구조 변경

--- src/api/project.js
+++ src/api/project.js
@@ -1,24 +1,11 @@
-import axios from "axios";
 import { store } from "../store/index.js";
+import apiClient from "./apiClient.js";
 
 export const createProject = async () => {
   const projectState = store.getState().project;
-  const accessToken = sessionStorage.getItem("access_token");
-
-  if (!accessToken) {
-    throw new Error("Access token is missing");
-  }
 
   try {
-    const response = await axios.post(
-      "/projects",
-      projectState,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const response = await apiClient.post("/projects", projectState);
     return response.data;
   } catch (error) {
     console.error("Error creating project:", error);
@@ -27,21 +14,11 @@ export const createProject = async () => {
 };
 
 export const updateFavorite = async (projectId) => {
-  const accessToken = sessionStorage.getItem("access_token"); // 토큰 가져오기
-
-  if (!accessToken) {
-    throw new Error("Access token is missing");
-  }
-  
   try {
-    const response = await axios.post(`/projects/${projectId}/favorite`, null, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    const response = await apiClient.post(`/projects/${projectId}/favorite`);
     return response.data;
   } catch (error) {
     console.error("Error favorite update:", error);
     throw error;
   }
-}
+};
